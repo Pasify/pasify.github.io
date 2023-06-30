97db0fa0cf6b13db38b32016198e5afe15655183
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2023-06-30 02:55:48 +0100

    second commit with major changes in layout, design and functionality

--- projects.js
+++ projects.js
@@ -1,38 +1,36 @@
 const Projects = [
   {
-    projectLink: "https://Mapty.netlify.app",
-    projectTitle: "mapty project4",
+    githubLink: "https://github.com/Pasify/Belsa-app",
+    projectLink: "https://pasify.github.io/Belsa-app/",
+    projectTitle: "Belsa App",
     projectDescription:
-            "A web application that allows to map your work out over a particular location",
-    projectTools:["ReactJs", "CSS/Sass"]
+      "A booking website with a sleek and responsive layout. Experience stunning design, intuitive interface, and engaging user experience in browsing and booking hotels, cars, fight and others.",
+    projectTools: ["ReactJs", "CSS/Sass"],
   },
   {
-    projectLink: "https://bankist.netlify.app",
-    projectTitle: "bankist app5",
+    githubLink: "https://github.com/Pasify/dicedash",
+    projectLink: "https://pasify.github.io/dicedash/",
+    projectTitle: "Dice-Dash",
     projectDescription:
-            "an app that allows you to borrow, money transfer money",
-    projectTools:["ReactJs", "CSS/Sass"]
+      "A simple dice game where players roll the dice, accumulate points, and aim to reach a target score. But watch out for rolling a '1'! Take turns, make strategic decisions, and be the first to reach the goal",
+    projectTools: ["ReactJs", "CSS/Sass", "JavaScript"],
   },
   {
-    projectLink: "https://bankist.netlify.app",
-    projectTitle: "bankist app6",
+    githubLink: "https://github.com/Pasify/Bankist-App",
+    projectLink: "https://pasify.github.io/Bankist-App/",
+
+    projectTitle: "Bankist App",
     projectDescription:
-            "an app that allows you to borrow, money transfer money",
-    projectTools:["ReactJs", "CSS/Sass"]
+      "A versatile banking app with balance tracking, transaction monitoring, interest accrual, and key features like borrowing, sending money, and secure account deletion. Experience seamless banking at your fingertips.",
+    projectTools: ["HTML", "CSS/Sass", "javaScript"],
   },
   {
-    projectLink: "https://bankist.netlify.app",
-    projectTitle: "bankist app7",
+    githubLink: "https://github.com/Pasify/Far-Away",
+    projectLink: "",
+    projectTitle: "Far Away",
     projectDescription:
-            "an app that allows you to borrow, money transfer money",
-    projectTools:["ReactJs", "CSS/Sass"]
-  },
-  {
-    projectLink: "https://bankist.netlify.app",
-    projectTitle: "bankist app8",
-    projectDescription:
-            "an app that allows you to borrow, money transfer money",
-    projectTools:["ReactJs", "CSS/Sass"]
+      "A travel checklist app built using the Vite development environment and React. It allows you to create a list of items you need to pack for your travels.",
+    projectTools: ["ReactJs", "CSS"],
   },
 ];
 export { Projects };
--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-"use strict"
+"use strict";
 import { Projects } from "./projects.js";
 
 // selectors///////////
 const checkbox = document.querySelector(".nav__check-box");
-const navList = document.querySelector(".nav__list")
-const navLink = document.querySelectorAll(".nav__link")
+const navList = document.querySelector(".nav__list");
+const navLink = document.querySelectorAll(".nav__link");
 const findOutBtn = document.querySelector("#findOut");
-const headerSection = document.querySelector("#header")
-const nav = document.querySelector(".nav")
+const headerSection = document.querySelector("#header");
+const nav = document.querySelector(".nav");
 const overlay = document.querySelector(".overlay");
 
 //tabbed component
-const tabs = document.querySelectorAll(".button__tab")
-const jobTabs= document.querySelector(".job-tabs")
-const jobDescriptionContent = document.querySelectorAll(".job__description-content");
+const tabs = document.querySelectorAll(".button__tab");
+const jobTabs = document.querySelector(".job-tabs");
+const jobDescriptionContent = document.querySelectorAll(
+  ".job__description-content"
+);
 
-const showMoreBtn = document.querySelector(".showMore")
+const showMoreBtn = document.querySelector(".showMore");
 const projectContainer = document.querySelector(".project__sub__container");
 const message = document.querySelector(".message");
 const messageOverlay = document.querySelector(".message-overlay");
 /////////////
-const allSection = document.querySelectorAll(".section")
-
+const allSection = document.querySelectorAll(".section");
 
 /////FUNCTIONALITY
 /////scroll bar functionality ///////
 const disableScroll = function () {
-    document.body.style.overflowY = "hidden";
-}
+  document.body.style.overflowY = "hidden";
+};
 const enableScroll = function () {
-    document.body.style.overflowY = "auto";
-}
+  document.body.style.overflowY = "auto";
+};
 //sticky navigation 1
-const navheight =Math.round( nav.getBoundingClientRect().height)
+const navheight = Math.round(nav.getBoundingClientRect().height);
 
 const observerFunc = function (entries) {
   const [entry] = entries;
@@ -43,44 +44,41 @@ const observerFunc = function (entries) {
   }
 };
 const observerOps = {
-    root: null,
-    threshold: 0,
-    rootMargin: `-${navheight}px`
-}
-const observer = new IntersectionObserver(observerFunc, observerOps)
-observer.observe(headerSection)
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navheight}px`,
+};
+const observer = new IntersectionObserver(observerFunc, observerOps);
+observer.observe(headerSection);
 
 //sticky navigation 2
 
 //smooth scroll into the sections//////
-const addHideOverlay = function(){
-    overlay.classList.add("hideoverlay");
-    checkbox.checked = false;
-    // document.body.style.overflowY = "auto";
-    enableScroll();
-}
+const addHideOverlay = function () {
+  overlay.classList.add("hideoverlay");
+  checkbox.checked = false;
+  // document.body.style.overflowY = "auto";
+  enableScroll();
+};
 
 navList.addEventListener("click", function (e) {
-    e.preventDefault()
-    if (e.target.classList.contains("nav__link")) {
-        let id = e.target.getAttribute("href");
-        
-        document.querySelector(id).scrollIntoView(
-            {behavior:"smooth"}
-            )
-        } 
-        
-        //collapses the nav
-        setTimeout(addHideOverlay, 300);
-        
-    })
-    
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    let id = e.target.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+
+  //collapses the nav
+  setTimeout(addHideOverlay, 300);
+});
+
 // resets the default of the resume button
 navList.querySelector(".resume").addEventListener("click", function (e) {
-    // e.cancelBubble = true;
-    // alert(`resume downloaded`)
-    e.stopImmediatePropagation()
-  addHideOverlay(); 
+  // e.cancelBubble = true;
+  // alert(`resume downloaded`)
+  e.stopImmediatePropagation();
+  addHideOverlay();
 });
 const NavOverlay = function () {
   if (this.checked == true) {
@@ -95,7 +93,7 @@ const NavOverlay = function () {
   }
 };
 checkbox.addEventListener("click", NavOverlay);
-overlay.addEventListener("click", addHideOverlay)
+overlay.addEventListener("click", addHideOverlay);
 /// find out more scroll
 findOutBtn.addEventListener("click", function () {
   document.querySelector(".about").scrollIntoView({
@@ -104,42 +102,42 @@ findOutBtn.addEventListener("click", function () {
 });
 ////////////////////////
 
-
 //navbar and overlay bar function on phone2
 //add event handler to each of the buttons in the tab by using event delegation, i.e targeting the parent element
 //tabbed component
 jobTabs.addEventListener("click", function (e) {
-    const clicked = e.target.closest(".button__tab")
-    if (!clicked) return
-    
-    //activate content area
-    //remove all active class on the element
-    jobDescriptionContent.forEach(cont => {
-        cont.classList.remove("describe--active");
-    })
-    //link
-    document.querySelector(`.job__description--${clicked.dataset.tab}`).classList.add("describe--active");
-})
+  const clicked = e.target.closest(".button__tab");
+  if (!clicked) return;
+
+  //activate content area
+  //remove all active class on the element
+  jobDescriptionContent.forEach((cont) => {
+    cont.classList.remove("describe--active");
+  });
+  //link
+  document
+    .querySelector(`.job__description--${clicked.dataset.tab}`)
+    .classList.add("describe--active");
+});
 
 //reveal sections  on scroll/////////////
 const revealFunct = function (entries, observer) {
-    const [entry] = entries
-    if (!entry.isIntersecting) {
-        return
-    }
-    entry.target.classList.remove("section-hidden")
-    observer.unobserve(entry.target)
-    
-}
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    return;
+  }
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
 const sectionObj = {
-    root: null,
-    threshold:0.20
-}
-let sectionObserver = new IntersectionObserver(revealFunct, sectionObj)
-allSection.forEach(sections => {
-    // sections.classList.add("section-hidden")
-    sectionObserver.observe(sections)
-})
+  root: null,
+  threshold: 0.2,
+};
+let sectionObserver = new IntersectionObserver(revealFunct, sectionObj);
+allSection.forEach((sections) => {
+  // sections.classList.add("section-hidden");
+  sectionObserver.observe(sections);
+});
 //////////////////////
 
 console.log(document.body);
@@ -147,52 +145,89 @@ console.log(document.body);
 
 // show more functionality /////////
 const projectMapped = Projects.map(function (project) {
-    return `<div class="project1 project">
-    <div class="icon-container">
-        <svg class="file-icon">
-            <use xlink:href="./images/sprite.svg#icon-folder-open-o" ></use>
-        </svg>
-        <a href=${project.projectLink}>
-            <svg class="link-icon">
-                <use xlink:href="./images/sprite.svg#icon-external-link"></use>
-            </svg>
-        </a>                            
-    </div>
-    <div class="project__details">
-        <h2 class="heading">${project.projectTitle}</h2>
-        <p>
-            ${project.projectDescription}
-        </p>
-        </div>
-    <div class="project__tools">
-        <span>${project.projectTools[0]}</span>
-         <span>${project.projectTools[1]}</span>
-    </div>
-</div>`;
+  return `
+      <li>
+                        <div class="project1 project">
+                            <div class="icon-container">
+                                <div class="project-folder">
+                                    <svg class="file-icon">
+                                        <use xlink:href="./images/sprite.svg#icon-folder-open-o"></use>
+                                    </svg>
+                                </div>
+                                
+                                <div class="project-links">
+                                    <a href="${
+                                      project.githubLink
+                                    }" class="github-link" target="_blank">
+                                        <svg class="link-icon">
+                                            <use xlink:href="./images/sprite.svg#icon-github">
+                                            </use>
+                                        </svg>
+                                    </a>
+                                    
+                                    ${
+                                      project.projectLink &&
+                                      `<a
+                                          href="${project.projectLink}"
+                                          class="external-link"
+                                          target="_blank"
+                                        >
+                                          <svg class="link-icon">
+                                            <use xlink:href="./images/sprite.svg#icon-external-link"></use>
+                                          </svg>
+                                        </a>`
+                                    }
+                                    
+                                </div>
+                            </div>
+                            <div class="project__details">
+                                <h2 class="heading">
+                                    <a href="${
+                                      project.projectLink
+                                    }" target="_blank"> ${project.projectTitle} </a>
+                                </h2>
+                                <p class="heading_brief">${
+                                  project.projectDescription
+                                }</p>
+                            </div>
+                            <div class="project__tools">
+                                ${project.projectTools
+                                  .map((tool) => `<span>${tool}</span>`)
+                                  .join(" ")}
+                            </div>
+                        </div>
+                        <!--  -->
+                    </li>
+  `;
 });
 
 let currCounter = 0;
 showMoreBtn.addEventListener("click", function () {
-    const showResult = 3;
-    for (let ind = 0; ind < showResult; ind++) {
-        if (currCounter > projectMapped.length) {
-            message.querySelector(
-              ".message__content"
-            ).textContent = `Oops!, That's all the project I have for now.
-            More projects coming soon!😉`;
-            message.classList.remove("hidemessage")
-            messageOverlay.classList.remove("hideoverlay")
-            disableScroll()
-            return;
-        }
-        projectContainer.insertAdjacentHTML("beforeend", projectMapped[ind + currCounter]);
+  const showResult = 3;
+  for (let ind = 0; ind < showResult; ind++) {
+    if (currCounter + ind < projectMapped.length) {
+      projectContainer.insertAdjacentHTML(
+        "beforeend",
+        projectMapped[ind + currCounter]
+      );
     }
-    currCounter += showResult
-    let closebtn = message.querySelector(".btn");
-    closebtn.addEventListener('click', function () {
-        message.classList.add("hidemessage");
-        messageOverlay.classList.add("hideoverlay");
-        enableScroll()
-    });
-})
+    if (currCounter > projectMapped.length) {
+      message.querySelector(
+        ".message__content"
+      ).textContent = `Oops!, That's all the project I have for now.
+            More projects coming soon!😉`;
+      message.classList.remove("hidemessage");
+      messageOverlay.classList.remove("hideoverlay");
+      disableScroll();
+      return;
+    }
+  }
+  currCounter += showResult;
+  let closebtn = message.querySelector(".btn");
+  closebtn.addEventListener("click", function () {
+    message.classList.add("hidemessage");
+    messageOverlay.classList.add("hideoverlay");
+    enableScroll();
+  });
+});
 //////////////////////
